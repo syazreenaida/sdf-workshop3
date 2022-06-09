884e6612cba7bb7a998e46e5225a799647c62bd3
syazreenaida <syazreenaida@gmail.com>
2022-06-09 09:28:06 +0800

    first commit

---
+++ src/main/java/vttp2batch2b/day03/workshop/App.java
@@ -0,0 +1,13 @@
+package vttp2batch2b.day03.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
